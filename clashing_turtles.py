349064d8fd8d2eb8bb4d89dbe2e3c78f8d32d1e3
vighv <vighv@users.noreply.github.com>
2016-09-26 21:53:01 -0400

    Now wall with both sides!

--- clashing_turtles.py
+++ clashing_turtles.py
@@ -11,28 +11,48 @@ import random
 
 Num_turtles = 2
 x1 = 0
+x2 = 0
 
 def where1(pose_data):
     global x1
     x1 = pose_data.x
+
+def where2(pose_data):
+    global x2
+    x2 = pose_data.x
+
+
 # Spawn and kill turtles to draw the boundary:
 
 def draw_boundary():
     rospy.wait_for_service('spawn')
-    draw1 = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
-    draw1(0,5.5,0,'artist1')
+
+    draw_artist = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
+    draw_artist(0, 5.5, 0, 'artist1')
+    draw_artist(6, 5.5, 0, 'artist2')
+
     artist1_set_pen = rospy.ServiceProxy('artist1/set_pen', turtlesim.srv.SetPen)
-    artist1_set_pen(255,0,0,2,0)
-    #rospy.init_node('subber_for_drawing')
+    artist1_set_pen(255, 0, 0, 2, 0)
+
+    artist2_set_pen = rospy.ServiceProxy('artist2/set_pen', turtlesim.srv.SetPen)
+    artist2_set_pen(255, 0, 0, 2, 0)
+
+    rospy.wait_for_service('kill')
+    kill_artist = rospy.ServiceProxy('kill', turtlesim.srv.Kill)
+
     rospy.Subscriber('artist1/pose',Pose,where1)
     global x1
     while x1 < 5:
         tpub = rospy.Publisher("artist1/cmd_vel", Twist, queue_size=10)
-        tpub.publish(Twist(Vector3(1, 0, 0), Vector3(0, 0, 0)))
+        tpub.publish(Twist(Vector3(10, 0, 0), Vector3(0, 0, 0)))
+    kill_artist('artist1')
 
-    rospy.wait_for_service('kill')
-    kill_first_artist = rospy.ServiceProxy('kill', turtlesim.srv.Kill)
-    kill_first_artist('artist1')
+    rospy.Subscriber('artist2/pose', Pose, where2)
+    global x2
+    while x2 < 11:
+        tpub2 = rospy.Publisher("artist2/cmd_vel", Twist, queue_size=10)
+        tpub2.publish(Twist(Vector3(10, 0, 0), Vector3(0, 0, 0)))
+    kill_artist('artist2')
     #rospy.spin()
     #rospy.signal_shutdown("Done drawing")
 
